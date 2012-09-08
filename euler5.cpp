093793c483c38e335c0edb20716af6591bdc10bb
Michael Kindt <mkindt@unca.edu>
2012-09-07 20:32:51 -0400

    test from 1 to 10 worked, output 2520, now 1 to 20, outputs 232792560, only barely good idea was starting 19*20

--- euler5.cpp
+++ euler5.cpp
@@ -2,18 +2,19 @@
 
 int main() {
 	//Euler problem 5
-	// 9*10 means divisible by 2,3,5
-	// 8 includes 4, 7...
-	int test = 9*10;
+	// 19*20 means divisible by 2,4,5,10
+	// start high: 18,17,16,15,14,13,12,11
+	// what's a better way to determine used divisors....
+	int test = 19*20;
 	int smallest = 0;
 	int count = 0;
-	for (int i=1; i<=test*i; ++i) {
+	for (int i = 1; i <= test*i; ++i) {
 		count = 0;
-		for (int j = 8; j>=7; --j) {
+		for (int j = 18; j >= 11; --j) {
 			if ((test*i)%j == 0)
 				count++;
 		}
-		if (count == 2) {
+		if (count == 8) {
 			smallest = test*i;
 			break;
 		}
